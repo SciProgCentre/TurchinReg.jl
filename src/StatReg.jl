b7a0af65ce5e12f311297f251ae6722210e8ef60
Tatyana Abramova <loooj58@gmail.com>
2019-09-03 17:28:23 +0300

    testing added

--- src/StatReg.jl
+++ src/StatReg.jl
@@ -4,9 +4,11 @@ module StatReg
     include("gauss_error.jl")
     include("vector.jl")
     include("config.jl")
+    include("mcmc.jl")
 
     export BaseFunction, Basis, omega, FourierBasis, CubicSplineBasis, LegendreBasis, BernsteinBasis
     export GaussErrorMatrixUnfolder, solve, GaussErrorUnfolder
     export PhiVec, call, errors
-    export config
+    export config, Config
+    export MCMCMatrixUnfolder, MCMCUnfolder, get_values
 end
